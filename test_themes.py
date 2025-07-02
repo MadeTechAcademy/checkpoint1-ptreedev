609f1981a54c73063446a61a1586d28f7e95383c
Peter Leaver <peteleaver@madetech.com>
2025-07-02 15:33:42 +0100

    tested print_duties method

--- test_themes.py
+++ test_themes.py
@@ -1,4 +1,13 @@
-from themes import x2
-def testIt():
-    #assert len(x2)>10
-    assert True is True
+import unittest
+from unittest.mock import patch
+
+from themes import print_duties, duty_list
+
+# are the tests and naming conventions verbose?
+
+def test_print_duties_prints_all_duties():
+    with patch("builtins.print") as mock_print:
+        print_duties()
+        assert mock_print.call_count == 13
+        for duty in duty_list:
+            mock_print.assert_any_call("{0}\n".format(duty))
