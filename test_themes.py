b326fe390ce18e427d5cca80085ff395dfdb3182
Peter Leaver <peteleaver@madetech.com>
2025-07-15 11:45:15 +0100

    Refactor tests

--- test_themes.py
+++ test_themes.py
@@ -1,7 +1,27 @@
 from unittest.mock import patch, mock_open, call
-from themes import print_duties, duty_list, write_duties, format_html
+from themes import print_duties, duty_list, write_duties, format_html, duty_dict
 
 # are the tests and naming conventions verbose?
+# def format_test_html():
+#     expected_lines = [
+#         "<html>",
+#         "    <head>",
+#         "        <meta charset=\"UTF-8\"/>",
+#         "        <title>Apprenticeship Duties</title>",
+#         "    </head>",
+#         "    <body>",
+#         "        <h1>Apprenticeship Duties</h1>",
+#         "        <ol>"
+#     ]
+#     expected_lines += [f"            <li>{duty}</li>" for duty in duty_list]
+#     expected_lines += [
+#         "        </ol>",
+#         "    </body>",
+#         "</html>"
+#     ]
+#     expected_html = "\n".join(expected_lines)
+#     return expected_html
+
 def format_test_html():
     expected_lines = [
         "<html>",
@@ -13,33 +33,36 @@ def format_test_html():
         "        <h1>Apprenticeship Duties</h1>",
         "        <ol>"
     ]
-    expected_lines += [f"            <li>{duty}</li>" for duty in duty_list]
+    for duty in duty_dict.values():
+        expected_lines.append(f"            <li>{duty}</li>")
     expected_lines += [
         "        </ol>",
         "    </body>",
         "</html>"
     ]
+
     expected_html = "\n".join(expected_lines)
     return expected_html
+
 
 def test_print_duties_prints_all_duties():
     with patch("builtins.print") as mock_print:
         print_duties(1)
         assert mock_print.call_count == 13
-        for duty in duty_list:
+        for duty in duty_dict.values():
             mock_print.assert_any_call("{0}\n".format(duty))
 
 def test_opt_call_security_prints_duty_9():
       with patch("builtins.print") as mock_print:
         print_duties(3)
         assert mock_print.call_count == 1
-        mock_print.assert_called_once_with(duty_list[8])
+        mock_print.assert_called_once_with(duty_dict[9])
 
 def test_opt_boot_camp_prints_duty_1_2_3_4_13():
       with patch("builtins.print") as mock_print:
         print_duties(4)
         assert mock_print.call_count == 5
-        calls = [call(duty_list[0]), call(duty_list[1]), call(duty_list[2]), call(duty_list[3]), call(duty_list[12])]
+        calls = [call(duty_dict[1]), call(duty_dict[2]), call(duty_dict[3]), call(duty_dict[4]), call(duty_dict[13])]
         mock_print.assert_has_calls(calls)
 
 def test_format_html():
